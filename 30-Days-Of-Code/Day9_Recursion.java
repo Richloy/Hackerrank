d1326eeba9a180efa828b105bdc7e1a9c66cc074
Richloy <richloy@gmail.com>
2017-02-23 10:27:27 +0000

    Day 9 - Recursion

--- 30-Days-Of-Code/Day9_Recursion.java
+++ 30-Days-Of-Code/Day9_Recursion.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Day9Recursion {
+public class Day9_Recursion {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
