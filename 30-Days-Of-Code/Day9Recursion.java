5acb2a63c450bf78bf3b9956a6c1f8131c43f4c5
Richloy <richloy@gmail.com>
2017-02-23 10:22:16 +0000

    Day 9 - Recursion

---
+++ 30-Days-Of-Code/Day9Recursion.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Day9Recursion {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+        System.out.print(factorial(N));
+    }
+    
+    public static int factorial(int N){
+        
+        if(N > 1){
+            return N*(factorial(N-1));        
+        }else{
+            return 1;
+        }
+    }
+}
