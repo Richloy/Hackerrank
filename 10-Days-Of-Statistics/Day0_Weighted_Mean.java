00be90f18f8d068ec7a6c618a5fdeb4c318ecb51
Richloy <richloy@gmail.com>
2017-03-14 17:45:02 +0000

    added 10 Days of Statistics Day 0

--- 10-Days-Of-Statistics/Day0_Weighted_Mean.java
+++ 10-Days-Of-Statistics/Day0_Weighted_Mean.java
@@ -7,6 +7,30 @@ import java.util.regex.*;
 public class Day0_Weighted_Mean {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int numerator = 0;
+		int denominator = 0;
+		int run = 2;
+		int[] X = new int[N];
+		int[] W = new int[N];
+		while (run > 0){
+			for (int i = 0; i < N; i++){
+				if (run == 2){
+					X[i] = sc.nextInt();
+				} else {
+					W[i] = sc.nextInt();
+				}
+			}
+			run--;
+		}
+		sc.close();
+		for (int i = 0; i < N; i++){
+			numerator += X[i]*W[i];
+			denominator += W[i];
+			
+		}
+		System.out.printf("%.1f\n",(double)numerator/denominator);
     }
 }
