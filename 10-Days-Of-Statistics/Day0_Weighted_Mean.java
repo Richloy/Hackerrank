5383c62f262ef3fc35757e3ec92f170ac8f9dc8f
Richloy <richloy@gmail.com>
2017-03-16 20:00:41 +0000

    added 10 Days of Statistics Day 0

--- 10-Days-Of-Statistics/Day0_Weighted_Mean.java
+++ 10-Days-Of-Statistics/Day0_Weighted_Mean.java
@@ -4,22 +4,33 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class SolveMeFirst {
+public class Day0_Weighted_Mean {
 
-    static int solveMeFirst(int a, int b) {
+    public static void main(String[] args) {
 		
-      return a+b;
-	}
-   
-	public static void main(String[] args) {
-		
-        Scanner in = new Scanner(System.in);
-        int a;
-        a = in.nextInt();
-        int b;
-        b = in.nextInt();
-        int sum;
-        sum = solveMeFirst(a, b);
-        System.out.println(sum);
-   }
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int numerator = 0;
+		int denominator = 0;
+		int run = 2;
+		int[] X = new int[N];
+		int[] W = new int[N];
+		while (run > 0){
+			for (int i = 0; i < N; i++){
+				if (run == 2){
+					X[i] = sc.nextInt();
+				} else {
+					W[i] = sc.nextInt();
+				}
+			}
+			run--;
+		}
+		sc.close();
+		for (int i = 0; i < N; i++){
+			numerator += X[i]*W[i];
+			denominator += W[i];
+			
+		}
+		System.out.printf("%.1f\n",(double)numerator/denominator);
+    }
 }
